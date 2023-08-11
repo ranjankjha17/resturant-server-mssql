16fd32b88b9752c8ca340ef0eb808b6b481c3074
Ranjan <talk2ranjanktr@gmail.com>
2023-08-11 12:34:24 +0530

    crete users api

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const bodyParser = require('body-parser')
 //const { Request } = require('tedious'); 
 const sql = require('mssql');
 const cors=require('cors')
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 app.use(cors())
+app.use(express.json());
 
 const dbConfig = {
   user: 'db_a9d5a9_resturant_admin',
@@ -21,7 +23,7 @@ const dbConfig = {
 };
 
 // API endpoint to fetch data
-app.get('/api/products', async (req, res) => {
+app.get('/api/allProducts', async (req, res) => {
   try {
     await sql.connect(dbConfig);
     const query = 'SELECT * FROM Item_master';
@@ -56,6 +58,45 @@ app.post('/api/order', async (req, res) => {
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'An error occurred while adding data.' });
+  } finally {
+    await sql.close();
+  }
+});
+
+
+// API endpoint for user registration
+app.post('/api/register', async (req, res) => {
+  try {
+    const {username,email, password } = req.body;
+   // let username="ranjan"
+    console.log(email)
+    await sql.connect(dbConfig);
+    
+    // Check if user with the given email already exists
+    const emailCheckQuery = 'SELECT * FROM Users WHERE Email = @email';
+    const emailCheckRequest = new sql.Request();
+    emailCheckRequest.input('email', sql.NVarChar, email);
+    const emailCheckResult = await emailCheckRequest.query(emailCheckQuery);
+    
+    if (emailCheckResult.recordset.length > 0) {
+      return res.status(400).json({ error: 'Email already registered.' });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Insert new user into the database
+    const insertQuery = `
+      INSERT INTO Users (Username, Email, Password)
+      VALUES (@username, @email, @hashedPassword)
+    `;
+    const insertRequest = new sql.Request();
+    insertRequest.input('username', sql.NVarChar, username);
+    insertRequest.input('email', sql.NVarChar, email);
+    insertRequest.input('hashedPassword', sql.NVarChar, hashedPassword);
+    await insertRequest.query(insertQuery);
+    
+    res.status(201).json({ message: 'User registered successfully.' });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred while registering user.' });
   } finally {
     await sql.close();
   }
