bf31aea3abf9f094ece407603ed250e2a93481c9
Ranjan <talk2ranjanktr@gmail.com>
2023-08-12 23:33:02 +0530

    add KOT No

--- productRoutes.js
+++ productRoutes.js
@@ -7,44 +7,67 @@ const sql = require('mssql');
 
 // API endpoint to fetch data
 router.get('/api/allProducts', async (req, res) => {
-    try {
-      await database.connect();
-      const query = 'SELECT * FROM Item_master';
-      const result = await sql.query(query);
-      res.json(result.recordset);
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(500).send('An error occurred while fetching data.');
-    } finally {
-      await database.close();
-    }
-  });
-  
-  // POST requests for order
-  router.post('/api/order', async (req, res) => {
-    try {
-      const {itemName,rate,qty,tableNo,itemCode} = req.body;
-      await database.connect();    
+  try {
+    await database.connect();
+    const query = 'SELECT * FROM Item_master';
+    const result = await sql.query(query);
+    res.json(result.recordset);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred while fetching data.');
+  } finally {
+    await database.close();
+  }
+});
+
+// POST requests for order
+
+router.post('/api/order', async (req, res) => {
+  const orders = req.body;
+  console.log(orders)
+  if (!orders || orders.length === 0) {
+    return res.status(400).json({ message: 'No order data provided' });
+  }
+  await database.connect();
+  try {
+    const transaction = new sql.Transaction();
+    await transaction.begin();
+    const request = new sql.Request(transaction);
+    const lastKOTNumberQuery = `
+    SELECT TOP 1 KOT_No
+    FROM Table_Day
+    ORDER BY KOT_No DESC
+  `;
+  const lastKOTNumberResult = await request.query(lastKOTNumberQuery);
+  const lastKOTNumber = lastKOTNumberResult.recordset[0].KOT_No || 0;
+  // Increment and generate the new roll number
+  const newKOTNumber = lastKOTNumber + 1;
+    //console.log(newKOTNumber)
+    for (const order of orders) {
+      let count=1
+      const { itemName, rate, qty, tableNo, itemCode } = order;
       const query = `
-        INSERT INTO Table_Day (ItemName, Rate, Qty,TableNo,ItemCode)
-        VALUES (@itemName, @rate, @qty,@tableNo,@itemCode)
-      `;    
-      const request = new sql.Request();
-      request.input('itemName', sql.VarChar, itemName); 
-      request.input('rate', sql.Float, rate); 
-      request.input('qty', sql.Float, qty); 
-      request.input('tableNo', sql.Int, tableNo); 
-      request.input('itemCode', sql.Int, itemCode); 
-      await request.query(query);    
-      res.status(201).json({ message: 'Order added successfully.' });
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ error: 'An error occurred while adding data.' });
-    } finally {
-      await database.close();
+      INSERT INTO Table_Day (ItemName, Rate, Qty, TableNo, ItemCode,KOT_No)
+      VALUES (@itemName_${itemCode}_${tableNo}, @rate_${itemCode}_${tableNo}, @qty_${itemCode}_${tableNo}, @tableNo_${itemCode}_${tableNo}, @itemCode_${itemCode}_${tableNo},@kotNumber_${itemCode}_${tableNo})
+    `;
+      request.input(`itemName_${itemCode}_${tableNo}`, sql.VarChar, itemName);
+      request.input(`rate_${itemCode}_${tableNo}`, sql.Float, rate);
+      request.input(`qty_${itemCode}_${tableNo}`, sql.Float, qty);
+      request.input(`tableNo_${itemCode}_${tableNo}`, sql.Int, tableNo);
+      request.input(`itemCode_${itemCode}_${tableNo}`, sql.Int, itemCode);
+      request.input(`kotNumber_${itemCode}_${tableNo}`, sql.Int, newKOTNumber);
+      await request.query(query);
+      count=count+1
     }
-  });
+    await transaction.commit();
+    return res.status(201).json({ message: 'Data inserted successfully' });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred while adding data.' });
+  } finally {
+    await database.close();
+  }
+});
 
 
-
-module.exports=router
+module.exports = router
