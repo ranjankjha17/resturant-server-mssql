c735493ab07ffca6f995590a70f3aeb8feabca4f
Ranjan <talk2ranjanktr@gmail.com>
2023-08-15 00:46:42 +0530

    modify order rest api

--- productRoutes.js
+++ productRoutes.js
@@ -22,43 +22,94 @@ router.get('/api/allProducts', async (req, res) => {
 
 // POST requests for order
 
+// router.post('/api/order', async (req, res) => {
+//   const orders = req.body;
+//   console.log(orders)
+//   if (!orders || orders.length === 0) {
+//     return res.status(400).json({ message: 'No order data provided' });
+//   }
+//   await database.connect();
+//   try {
+//     const transaction = new sql.Transaction();
+//     await transaction.begin();
+//     const request = new sql.Request(transaction);
+//     const lastKOTNumberQuery = `
+//     SELECT TOP 1 KOT_No
+//     FROM Table_Day
+//     ORDER BY KOT_No DESC
+//   `;
+//   const lastKOTNumberResult = await request.query(lastKOTNumberQuery);
+//   const lastKOTNumber = lastKOTNumberResult.recordset[0].KOT_No || 0;
+//   // Increment and generate the new roll number
+//   const newKOTNumber = lastKOTNumber + 1;
+//     //console.log(newKOTNumber)
+//     for (const order of orders) {
+//       let count=1
+//       const { itemName, rate, qty, tableNo, itemCode } = order;
+//       const query = `
+//       INSERT INTO Table_Day (ItemName, Rate, Qty, TableNo, ItemCode,KOT_No)
+//       VALUES (@itemName_${itemCode}_${tableNo}, @rate_${itemCode}_${tableNo}, @qty_${itemCode}_${tableNo}, @tableNo_${itemCode}_${tableNo}, @itemCode_${itemCode}_${tableNo},@kotNumber_${itemCode}_${tableNo})
+//     `;
+//       request.input(`itemName_${itemCode}_${tableNo}`, sql.VarChar, itemName);
+//       request.input(`rate_${itemCode}_${tableNo}`, sql.Float, rate);
+//       request.input(`qty_${itemCode}_${tableNo}`, sql.Float, qty);
+//       request.input(`tableNo_${itemCode}_${tableNo}`, sql.Int, tableNo);
+//       request.input(`itemCode_${itemCode}_${tableNo}`, sql.Int, itemCode);
+//       request.input(`kotNumber_${itemCode}_${tableNo}`, sql.Int, newKOTNumber);
+//       await request.query(query);
+//       count=count+1
+//     }
+//     await transaction.commit();
+//     return res.status(201).json({ message: 'Data inserted successfully' });
+//   } catch (error) {
+//     console.error('Error:', error);
+//     res.status(500).json({ error: 'An error occurred while adding data.' });
+//   } finally {
+//     await database.close();
+//   }
+// });
+
 router.post('/api/order', async (req, res) => {
   const orders = req.body;
-  console.log(orders)
+  console.log(orders);
+  
   if (!orders || orders.length === 0) {
     return res.status(400).json({ message: 'No order data provided' });
   }
+  
   await database.connect();
+  
   try {
     const transaction = new sql.Transaction();
     await transaction.begin();
     const request = new sql.Request(transaction);
+    
     const lastKOTNumberQuery = `
-    SELECT TOP 1 KOT_No
-    FROM Table_Day
-    ORDER BY KOT_No DESC
-  `;
-  const lastKOTNumberResult = await request.query(lastKOTNumberQuery);
-  const lastKOTNumber = lastKOTNumberResult.recordset[0].KOT_No || 0;
-  // Increment and generate the new roll number
-  const newKOTNumber = lastKOTNumber + 1;
-    //console.log(newKOTNumber)
-    for (const order of orders) {
-      let count=1
-      const { itemName, rate, qty, tableNo, itemCode } = order;
-      const query = `
-      INSERT INTO Table_Day (ItemName, Rate, Qty, TableNo, ItemCode,KOT_No)
-      VALUES (@itemName_${itemCode}_${tableNo}, @rate_${itemCode}_${tableNo}, @qty_${itemCode}_${tableNo}, @tableNo_${itemCode}_${tableNo}, @itemCode_${itemCode}_${tableNo},@kotNumber_${itemCode}_${tableNo})
+      SELECT TOP 1 KOT_No
+      FROM Table_Day
+      ORDER BY KOT_No DESC
     `;
-      request.input(`itemName_${itemCode}_${tableNo}`, sql.VarChar, itemName);
-      request.input(`rate_${itemCode}_${tableNo}`, sql.Float, rate);
-      request.input(`qty_${itemCode}_${tableNo}`, sql.Float, qty);
-      request.input(`tableNo_${itemCode}_${tableNo}`, sql.Int, tableNo);
-      request.input(`itemCode_${itemCode}_${tableNo}`, sql.Int, itemCode);
-      request.input(`kotNumber_${itemCode}_${tableNo}`, sql.Int, newKOTNumber);
+    
+    const lastKOTNumberResult = await request.query(lastKOTNumberQuery);
+    const lastKOTNumber = lastKOTNumberResult.recordset[0].KOT_No || 0;
+    const newKOTNumber = lastKOTNumber + 1;
+    
+    for (const order of orders) {
+      const { itemName, rate, qty, tableNo, itemCode } = order;
+      const paramNamePrefix = `${itemCode}_${tableNo}`; // Create a unique prefix
+      const query = `
+        INSERT INTO Table_Day (ItemName, Rate, Qty, TableNo, ItemCode, KOT_No)
+        VALUES (@itemName_${paramNamePrefix}, @rate_${paramNamePrefix}, @qty_${paramNamePrefix}, @tableNo_${paramNamePrefix}, @itemCode_${paramNamePrefix}, @kotNumber_${paramNamePrefix})
+      `;
+      request.input(`itemName_${paramNamePrefix}`, sql.VarChar, itemName);
+      request.input(`rate_${paramNamePrefix}`, sql.Float, rate);
+      request.input(`qty_${paramNamePrefix}`, sql.Float, qty);
+      request.input(`tableNo_${paramNamePrefix}`, sql.Int, tableNo);
+      request.input(`itemCode_${paramNamePrefix}`, sql.Int, itemCode);
+      request.input(`kotNumber_${paramNamePrefix}`, sql.Int, newKOTNumber);
       await request.query(query);
-      count=count+1
     }
+    
     await transaction.commit();
     return res.status(201).json({ message: 'Data inserted successfully' });
   } catch (error) {
