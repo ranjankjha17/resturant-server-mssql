c2eead5eb3446bda4559e31e97f4592dd2492fa5
Ranjan <talk2ranjanktr@gmail.com>
2023-08-24 20:43:35 +0530

    set printr apis

--- tableTypeRoutes.js
+++ tableTypeRoutes.js
@@ -19,6 +19,37 @@ try{
   }
 })
 
+router.get('/api/table',async(req,res)=>{
+  try{
+      await db.connect()
+      const query='SELECT * FROM Table_Mstr'
+      const result=await sql.query(query)
+      res.json(result.recordset)
+  
+  } catch (error) {
+      console.error('Error:', error);
+      res.status(500).send('An error occurred while fetching data.');
+    } finally {
+      await db.close();
+    }
+  })
+  
+  router.get('/api/printr',async(req,res)=>{
+    try{
+        await db.connect()
+        const query='SELECT * FROM PrintrSetting'
+        const result=await sql.query(query)
+        res.json(result.recordset)
+    
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred while fetching PrinterSetting data.');
+      } finally {
+        await db.close();
+      }
+    })
+    
+      
 
 
 module.exports=router
