0ce6a8caa713833208f169757aa070d5e99be6c5
Ranjan <talk2ranjanktr@gmail.com>
2023-09-02 17:51:47 +0530

    add time

--- productRoutes.js
+++ productRoutes.js
@@ -95,12 +95,12 @@ router.post('/api/order', async (req, res) => {
     const newKOTNumber = lastKOTNumber + 1;
 
     for (const order of orders) {
-      const { itemName, rate, qty, tableNo, itemCode, userID, currentTableType, amount, bookingDate } = order;
+      const { itemName, rate, qty, tableNo, itemCode, userID, currentTableType, amount, bookingDate,currentTime } = order;
       const Kot_Type = 'KOT'
       const paramNamePrefix = `${itemCode}_${tableNo}`;
       const query = `
-        INSERT INTO Table_Day (ItemName, Rate, Qty, TableNo, ItemCode, KOT_No,Amount,Login,TableType,KOT_Type,BDate)
-        VALUES (@itemName_${paramNamePrefix}, @rate_${paramNamePrefix}, @qty_${paramNamePrefix}, @tableNo_${paramNamePrefix}, @itemCode_${paramNamePrefix}, @kotNumber_${paramNamePrefix},@amount_${paramNamePrefix},@userID_${paramNamePrefix},@currentTableType_${paramNamePrefix},@Kot_Type_${paramNamePrefix},@bookingDate_${paramNamePrefix})
+        INSERT INTO Table_Day (ItemName, Rate, Qty, TableNo, ItemCode, KOT_No,Amount,Login,TableType,KOT_Type,BDate,BTime)
+        VALUES (@itemName_${paramNamePrefix}, @rate_${paramNamePrefix}, @qty_${paramNamePrefix}, @tableNo_${paramNamePrefix}, @itemCode_${paramNamePrefix}, @kotNumber_${paramNamePrefix},@amount_${paramNamePrefix},@userID_${paramNamePrefix},@currentTableType_${paramNamePrefix},@Kot_Type_${paramNamePrefix},@bookingDate_${paramNamePrefix},@currentTime_${paramNamePrefix})
       `;
       request.input(`itemName_${paramNamePrefix}`, sql.VarChar, itemName);
       request.input(`rate_${paramNamePrefix}`, sql.Float, rate);
@@ -109,6 +109,7 @@ router.post('/api/order', async (req, res) => {
       request.input(`itemCode_${paramNamePrefix}`, sql.Int, itemCode);
       request.input(`kotNumber_${paramNamePrefix}`, sql.Int, newKOTNumber);
       request.input(`bookingDate_${paramNamePrefix}`, sql.DateTime, bookingDate);
+      request.input(`bookingTime_${paramNamePrefix}`,sql.VarChar,currentTime);
       request.input(`userID_${paramNamePrefix}`, sql.VarChar, userID);
       request.input(`currentTableType_${paramNamePrefix}`, sql.VarChar, currentTableType);
       request.input(`amount_${paramNamePrefix}`, sql.Float, amount);
